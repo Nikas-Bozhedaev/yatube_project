2511f5947e6237dedf79e1cb20d783917ec7e434
Никита <pozidaevnikita85@gmail.com>
2023-02-01 21:02:23 +0300

    Dont Zalupa

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Main Page
 def index(request):
-    template = 'templates/posts/index.html'
+    template = 'posts/index.html'
     return render(request, template)
 
 def group_posts(request, slug):
