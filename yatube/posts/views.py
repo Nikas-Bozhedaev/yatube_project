516cc3c731a43f0df687fb0fb2921f3e2fa79adc
Никита <pozidaevnikita85@gmail.com>
2023-02-01 20:11:14 +0300

    Залупа

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Main Page
 def index(request):
-    return HttpResponse('Добро пожаловать в новую социальную сеть!')
+    template = 'templates/posts/index.html'
+    return render(request, template)
 
 def group_posts(request, slug):
     return HttpResponse(f'Здесь будет любая группа {slug}')
